92442eefb3beac17feaada1131cee6a2afa9acfb
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-04-11 20:49:34 +0300

    enable ionic icon set

--- client/src/quasar-user-options.js
+++ client/src/quasar-user-options.js
@@ -1,9 +1,12 @@
 import "./styles/quasar.scss";
-import "@quasar/extras/roboto-font/roboto-font.css";
-import "@quasar/extras/material-icons/material-icons.css";
+import iconSet from "quasar/icon-set/ionicons-v4";
+import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+
 import { Notify } from "quasar";
 
 export default {
-  config: {},
+  config: {
+    iconSet,
+  },
   plugins: { Notify },
 };
--- client/vue.config.js
+++ client/vue.config.js
@@ -23,4 +23,16 @@ module.exports = defineConfig({
       },
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: (tag) => tag.startsWith("ion-"),
+        };
+        return options;
+      });
+  },
 });
